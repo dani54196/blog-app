cf68a0df2400ee61faf74ff4a017f98dca20220f
dani54196 <dani54196@gmail.com>
2022-11-25 00:24:10 -0300

    db connection

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,25 @@
 require("dotenv").config();
 const express = require("express");
+const morgan = require("morgan");
 const cors = require("cors");
+const helmet = require("helmet");
+
+const IndexRoutes = require("./app/routes/index.routes.js");
 
 const app = express();
+// settings
+const PORT = process.env.NODE_DOCKER_PORT || 8080;
+var corsOptions = { origin: "http://localhost:8081" };
 
-var corsOptions = {
-  origin: "http://localhost:8081"
-};
-
+// middlewares
 app.use(cors(corsOptions));
+app.use(morgan("dev"));
+app.use(express.json()); // parse requests of content-type - application/json
+app.use(express.urlencoded({ extended: true }));// parse requests of content-type - application/x-www-form-urlencoded
+// routes
+app.use("/", IndexRoutes);
 
-// parse requests of content-type - application/json
-app.use(express.json());
-
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true }));
-
+// db
 const db = require("./app/models");
 
 db.sequelize.sync();
@@ -24,15 +28,9 @@ db.sequelize.sync();
 //   console.log("Drop and re-sync db.");
 // });
 
-// simple route
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to app!" });
-});
-
 require("./app/routes/turorial.routes")(app);
 
-// set port, listen for requests
-const PORT = process.env.NODE_DOCKER_PORT || 8080;
+// server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
